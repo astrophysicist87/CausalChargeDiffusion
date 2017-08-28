7aa89bbbc412aa976f4e016ccc3e878f11b8d98b
Christopher Plumberg <astrophysicist87@gmail.com>
2017-08-28 12:32:39 -0500

    Added special function library (not fully updated yet)

---
+++ special_function_library/cmlib/cmlib.h
@@ -0,0 +1,19 @@
+/*  cmlib.h header of useful definitions and includes
+
+from C Mathematical Function Handbook by Louis Baker
+Copyright 1991 by Louis Baker. All rights reserved.
+*/
+
+#ifndef CMLIB_H
+#define CMLIB_H
+
+#include <math.h>
+
+#define pi 3.141592653589793238462643383279
+#define  Egamma 0.577215664901532860606512090082402431
+#define abs(x) ((x)>0.? (x): -(x))
+#define errorcode -1.e60
+#define ierrorcode -255
+#define infinite_loop for(;;)
+
+#endif
